9d5ce5bf06f1ee319df4a90065fac1fd6bc1badc
nikos <nikos_solis@yahoo.com>
2025-04-05 16:39:18 -0500

    proyecto completo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import  React from 'react';
 import './App.css'
-import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { Index } from './components/home/home';
+import { Jugador } from './components/jugadores/jugador';
+import { Show } from './components/jugadores/mostrar';
+import { Temp } from './components/finanzas/fin'
 
 
 
@@ -12,7 +15,9 @@ export function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-
+        <Route path="/players" element={<Show />} />
+        <Route path="/add" element={<Jugador />} />
+        <Route path="/temp" element={<Temp />} />
       </Routes>
       </HashRouter>
 
